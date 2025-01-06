869e794445c24fcd25fc847e8dc43dfa02c4abf7
ChrisW0524 <chris.wang.wcy@gmail.com>
2025-01-05 22:52:23 -0800

    Implemented basic dnd kanban

--- app/page.tsx
+++ app/page.tsx
@@ -15,16 +15,6 @@ const habits = [
 ];
 export default function Home() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center space-y-4">
-      <h1 className="text-2xl">
-        <span className="text-wdbBlue">Web Development</span> at Berkeley
-      </h1>
-      <p>
-        Full-stack web development project template. Check the README for more
-        information!
-      </p>
-      <CustomSwitch />
       <HabitTracker habits={habits}/>
-    </div>
   );
 }
